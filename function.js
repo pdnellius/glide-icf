64781b479b23cf721ecae68965e15eafa7b4cdd1
rpetitto <r.petitto@gmail.com>
2024-03-26 23:28:35 -0400

    Update function.js

--- function.js
+++ function.js
@@ -1,17 +1,14 @@
 function generateAvatars(imageURLs) {
-  // Split the input string into an array of URLs
-  const urls = imageURLs.split(',');
+  // Split the input string into an array of URLs and trim whitespace
+  const urls = imageURLs.split(',').map(url => url.trim());
 
-  // Start the HTML string with a div to contain the avatars
-  let htmlOutput = '<div style="display: flex; flex-direction: row; align-items: center;">';
-
-  // Loop through each URL to generate the HTML for each avatar
-  urls.forEach((url, index) => {
-    htmlOutput += `<img src="${url.trim()}" style="border-radius: 50%; width: 50px; height: 50px; margin-left: ${index > 0 ? '-15px' : '0px'};" />`;
+  // Use an array to collect HTML snippets for better performance on large arrays
+  const htmlSnippets = urls.map((url, index) => {
+    return `<img src="${url}" alt="Avatar ${index + 1}" style="border-radius: 50%; width: 50px; height: 50px; margin-left: ${index > 0 ? '-15px' : '0px'};" />`;
   });
 
-  // Close the container div
-  htmlOutput += '</div>';
+  // Construct the final HTML string
+  const htmlOutput = `<div style="display: flex; flex-direction: row; align-items: center;">${htmlSnippets.join('')}</div>`;
 
   return htmlOutput;
 }
