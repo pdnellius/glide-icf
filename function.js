d69ea21be740decc1aade3a94da59da0f1c530b8
rpetitto <r.petitto@gmail.com>
2024-03-26 23:53:07 -0400

    Update function.js

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
 window.function = function(imageURLs,size) {
   // Split the input string into an array of URLs and trim whitespace
   const urls = imageURLs.value.split(',').map(url => url.trim());
-  const size = size.value;
+  const imagesize = size.value;
   
   // Use an array to collect HTML snippets for better performance on large arrays
   const htmlSnippets = urls.map((url, index) => {
-    return `<img src="${url}" alt="Avatar ${index + 1}" style="border-radius: 50%; width: ${size}px; height: ${size}px; margin-left: ${index > 0 ? '-15px' : '0px'};" />`;
+    return `<img src="${url}" alt="Avatar ${index + 1}" style="border-radius: 50%; width: ${imagesize}px; height: ${imagesize}px; margin-left: ${index > 0 ? '-15px' : '0px'};" />`;
   });
 
   // Construct the final HTML string
